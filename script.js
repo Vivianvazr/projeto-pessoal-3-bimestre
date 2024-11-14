d7812125ed5c6065781bc506d8db0ab0767543f7
Vívian Vaz <Vivianvaz5125@gmail.com>
2024-11-14 07:43:43 -0300

    aa

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ menu = prompt(`opção disponível
     3- sair`)
     switch(menu){
         case'1': 
-      alert('`Eu gosto de assistir filmes e séries, gosto de jogar de handbol, tenho muitas comidas preferidas, pizza, lasanha, açaí, purê de batata e muitas outras.`')
+      alert('`Eu gosto de assistir filmes e séries, gosto de jogar de handbol, tenho muitas comidas preferidas, pizza, lasanha, açaí e muitas outras.`')
         break
         case'2': 
      alert('Meu nome completo é Vívian Maria Vaz Ribeiro, eu tenho 15 anos, nasci em 06 de março de 2009, em Caucaia, tenho 1,55 de altura e moro em General Sampaio.')
